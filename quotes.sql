ff5034d24b50fa7e51a0564108cf3eca596715e0
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-10 18:34:44 -0800

    first commit

---
+++ quotes.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2022 at 03:29 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `“quotesdb”`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quotes`
+--
+
+CREATE TABLE `quotes` (
+  `id` int(11) NOT NULL,
+  `quote` text NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `quotes`
+--
+
+INSERT INTO `quotes` (`id`, `quote`, `authorId`, `categoryId`) VALUES
+(1, '“Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.”', 1, 1),
+(2, '“I love being married. It’s so great to find that one special person you want to annoy for the rest of your life.”', 3, 1),
+(3, '\"The best way to predict the future is to create it\"', 5, 4),
+(4, '\"The best and most beautiful things in the world can not be seen or even touched. They must be felt with the heart', 4, 4),
+(5, '“Do I not destroy my enemies when I make them my friends?” ', 5, 4),
+(6, '\"He who is devoid of the power to forgive is devoid of the power to love\"', 6, 3),
+(7, '\"Where there is love there is life\"', 7, 3),
+(8, '\"Life is either a daring adventure or nothing\"', 4, 2),
+(9, '\"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle\"', 8, 2),
+(10, '\"There is more to life than simply increasing its speed\"', 7, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `quotes`
+--
+ALTER TABLE `quotes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quotes_categoryid_fk` (`categoryId`),
+  ADD KEY `quotes_authorid_fk` (`authorId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `quotes`
+--
+ALTER TABLE `quotes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quotes`
+--
+ALTER TABLE `quotes`
+  ADD CONSTRAINT `quotes_authorid_fk` FOREIGN KEY (`authorId`) REFERENCES `authors` (`id`),
+  ADD CONSTRAINT `quotes_categoryid_fk` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
