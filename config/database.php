98cb5e00ce29559b122b7e257ecbafbc43e5c9c5
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-18 15:20:52 -0700

    troubleshooting db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,34 @@
-<?php 
+<?php  
+//*****NEED TO SWITCH TO ALL ENV VARIABLES */
 
+class Database {
+    
+    private $host = 'm7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $user = 'w6zf2cbuuhpaanb5';
+    private $password = getenv($password);
+    private $dbname = 'ttyw871lavputc97';
+    private $connection;
+
+    public function dbConnect() {
+        this-$connection = null; //disconnect from any previous connection
+    
+        try {
+            $connection = new PDO("mysql:host= {$this->$host};dbname= {$this->$dbname}", $this->$user, $this->$password);
+            //set PDO error mode to exception
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected to Database";
+        } catch (PDOexception $e) {
+            echo "Conection to Database Failed:" . $e->getMessage();
+            exit();
+        }
+        return $this-> $connection; 
+    }
+}
+?>
+
+
+//**
+<?php 
 class Database {
     
     private $connection;
@@ -30,3 +59,7 @@ class Database {
     }
 }
 ?>
+*/
+
+
+
