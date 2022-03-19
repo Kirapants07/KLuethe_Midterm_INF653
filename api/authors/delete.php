b24ee3e362828002b99b87ee796f95a3d5449d05
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-18 20:12:35 -0700

    added delete.php

---
+++ api/authors/delete.php
@@ -0,0 +1,28 @@
+<?php
+require '../../config/Database.php';
+require '../../model/Author.php';
+
+header('Access-Control-Allow-Methods: DELETE');
+
+//Instantiate db and connect
+$database = new Database();
+$db = $database->dbConnect();
+
+//Instantiate author object
+$newAuthor = new Author($db);
+
+//get posted data (id)
+$data = json_decode(file_get_contents("php://input"));
+
+//set ID
+$newAuthor->id = $data->id;
+
+//Check if delete author entry was successful
+if ($newAuthor->delete()) {
+    echo json_encode(array('message' => 'Post deleted'))
+}
+else {
+    echo json_encode(array('message' => 'Error: Post not deleted'))
+}
+exit(); //prevent accidentally attempting to complete more than one operation per HTTP request
+?>
