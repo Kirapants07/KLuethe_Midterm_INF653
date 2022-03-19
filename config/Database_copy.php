4c686f944e210fe532d34828a326e92a7a5a8a32
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-19 14:23:53 -0700

    added new PDO

--- config/Database_copy.php
+++ config/Database_copy.php
@@ -18,15 +18,15 @@ class Database {
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
 
-        // try {
-        //     $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-        //     // set the PDO error mode to exception
-        //     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //     echo "Connected successfully";
-        // } catch(PDOException $e) {
-        //     echo "Connection failed: " . $e->getMessage();
-        // }
-        // return $this->conn;
+        try {
+            $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+/*             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        return $this->conn; */
     }
 }
 ?>
