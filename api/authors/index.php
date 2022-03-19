4fd3491bdb89eca0674d3ae6c2364552ead0f314
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-19 15:34:22 -0700

    rename DB DataBase

--- api/authors/index.php
+++ api/authors/index.php
@@ -9,7 +9,7 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
 
-require_once '../../config/Database.php'; 
+require_once '../../config/DataBase.php'; 
 //require_once '../../config/Database_copy.php'; 
 require_once '../../model/Author.php';
 
