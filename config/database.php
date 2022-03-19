f1a086db6eb10661f0d9c271c49a1f60217dbc0a
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-19 09:15:10 -0700

    troubleshooting Database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,9 @@
 <?php 
 class Database {
-    private $url;
-
-
+    private $conn;
 
     public function connect() {
-        $this->url = getenv('JAWSDB_URL');
+        $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
 
         $hostname = $dbparts['host'];
@@ -13,14 +11,14 @@ class Database {
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
 
-        $conn = null; //disconnect from any previous connection
+        $this->conn = null; //disconnect from any previous connection
     
         try {
-            $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
             // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
-            return $conn;
+            return $this->conn;
         } catch(PDOException $e) {
             echo "Connection to Database Failed:" . $e->getMessage();
         }
