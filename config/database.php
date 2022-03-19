3049ea17c2057916108155bb9cc40e9eab6e0d79
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-19 09:52:12 -0700

    db

--- config/database.php
+++ config/database.php
@@ -1,28 +1,33 @@
 <?php 
-  class Database {
-    // DB Params
-
+class Database {
     private $conn;
+    private $url;
 
-    // DB Connect
-    public function connect() {
-      $this->conn = null;
-
-      $url = getenv('JAWSDB_URL');
-      $dbparts = parse_url($url);
-  
-      $hostname = $dbparts['host'];
-      $username = $dbparts['user'];
-      $password = $dbparts['pass'];
-      $database = ltrim($dbparts['path'],'/');
-
-      try { 
-        $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
-      }
-
-      return $this->conn;
+    function __construct(){
+        $this->url = getenv('JAWSDB_URL');
     }
-  }
+
+    public function connect() {
+
+        $dbparts = parse_url($this->url);
+
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+
+        $this->conn = null; //disconnect from any previous connection
+    
+        try {
+            $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
+
