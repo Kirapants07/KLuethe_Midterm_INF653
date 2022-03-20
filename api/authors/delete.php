bf826e5008f37f3bd82c061978c787546f2b5bbc
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-20 12:52:13 -0700

    added to delete.php

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -13,15 +13,19 @@ $newAuthor = new Author($db);
 //get posted data (id)
 $data = json_decode(file_get_contents("php://input"));
 
-//set ID
-$newAuthor->id = $data->id;
+//check if id is specified
+if (isset($data->id) && !empty($data->id))
+{
+    //assign variables
+    $newAuthor->id = $data->id;
+}
 
 //Check if delete author entry was successful
 if ($newAuthor->delete()) {
-    echo json_encode(array('message' => 'Author deleted'))
+    echo json_encode(array('message' => $newAuthor->id));
 }
 else {
-    echo json_encode(array('message' => 'Error: Author not deleted'))
+    echo json_encode(array('message' => 'No Quotes Found’));
 }
 exit(); //prevent accidentally attempting to complete more than one operation per HTTP request
 ?>
