5a71440fd6f587d20bb99fa80e0ac2daf771e765
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-20 10:30:16 -0700

    removed connected successfully message from Database.php

--- config/Database.php
+++ config/Database.php
@@ -21,7 +21,6 @@ class Database {
             $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
         } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
