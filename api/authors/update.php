a864e7a6888e892d2e9f08c45b2b22e2b96a973b
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-20 13:15:29 -0700

    update.php

--- api/authors/update.php
+++ api/authors/update.php
@@ -13,20 +13,25 @@ $newAuthor = new Author($db);
 //get posted data
 $data = json_decode(file_get_contents("php://input"));
 
-//set ID
-$newAuthor->id = $data->id;
 
-//need to use isset() to check if author is being sent ************************************
-//assign variables
-$newAuthor->id = $data->id;
-$newAuthor->author = $data->author;
+//check if author name is specified
+if (isset($data->author) && !empty($data->author))
+{
+    //assign variables
+    $newAuthor->id = $data->id;
+    $newAuthor->author = $data->author;
+}
 
-//Check if update author entry was successful
+//update author entry
 if ($newAuthor->update()) {
-    echo json_encode(array('message' => 'Post updated'));
+    echo json_encode(
+        array('author' => $newAuthor->author,
+                'id' => $newAuthor->id,
+        ));
 }
 else {
-    echo json_encode(array('message' => 'Error: Post not updated'));
+    echo json_encode(array('message' => 'authorId Not Found'));
 }
+
 exit(); //prevent accidentally attempting to complete more than one operation per HTTP request
 ?>
