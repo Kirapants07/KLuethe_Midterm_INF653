245173d2be5d0b5711cdc30b11792504952a8b32
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-21 12:01:34 -0700

    changed delete to only json, no array for single

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -3,6 +3,13 @@
 require_once '../../config/Database.php';
 require_once '../../model/Author.php';
 
+//Instantiate db and connect
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate author object
+$newAuthor = new Author($db);
+
 //get posted data (id)
 $data = json_decode(file_get_contents("php://input"));
 
@@ -15,10 +22,10 @@ if (isset($data->id) && !empty($data->id))
 
 //Check if delete author entry was successful
 if ($newAuthor->delete()) {
-    echo json_encode(array('id' => $newAuthor->id));
+    echo json_encode('id' => $newAuthor->id);
 }
 else {
-    echo json_encode(array('message' => 'No Quotes Found'));
+    echo json_encode('message' => 'No Quotes Found');
 }
 exit(); //prevent accidentally attempting to complete more than one operation per HTTP request
 ?>
