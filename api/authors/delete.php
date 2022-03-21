276d3386fb9c496366ef18b1654e6740c5077de8
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-21 11:48:33 -0700

    create.php working and screening missing parameters on local host

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -3,13 +3,6 @@
 require_once '../../config/Database.php';
 require_once '../../model/Author.php';
 
-//Instantiate db and connect
-$database = new Database();
-$db = $database->connect();
-
-//Instantiate author object
-$newAuthor = new Author($db);
-
 //get posted data (id)
 $data = json_decode(file_get_contents("php://input"));
 
--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -3,13 +3,6 @@
 require_once '../../config/Database.php';
 require_once '../../model/Author.php';
 
-//Instantiate db and connect
-$database = new Database();
-$db = $database->connect();
-
-//Instantiate author object
-$newAuthor = new Author($db);
-
 //Get ID from URL. If no id is set, then do nothing
 $newAuthor->id = $_GET['id'];
 
